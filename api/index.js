595486ce72b5060993b8afa8d81627c932897185
ismailgraphix <ismailgrapix@gmail.com>
2024-02-15 19:55:11 +0100

    database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from 'express'
+import mongoose from 'mongoose';
 
+mongoose.connect('mongodb+srv://ismailgrapix:ismailgrapix2020@blog-app.q9joetr.mongodb.net/blog-app?retryWrites=true&w=majority')
 const app = express();
 app.listen(3000, ()=>{
     console.log('server is running on port 3000')
