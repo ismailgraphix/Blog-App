0639ccfea808769b849026e31f76aff9b656a7eb
ismailgraphix <ismailgrapix@gmail.com>
2024-02-23 11:25:08 +0100

    complete update user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -33,6 +33,7 @@ export const test = (req, res) => {
           errorHandler(400, 'Username can only contain letters and numbers')
         );
       }
+    }
       try {
         const updatedUser = await User.findByIdAndUpdate(
           req.params.userId,
@@ -40,16 +41,16 @@ export const test = (req, res) => {
             $set: {
               username: req.body.username,
               email: req.body.email,
-              profilePicture: req.body.profilePicture, 
+              profilePicture: req.body.profilePicture,
               password: req.body.password,
-            },
+            }
           },
-          { new: true }
-        ); 
-        const { password, ...rest } = updatedUser._doc;
-        res.status(200).json(rest);
-      } catch (error) {
-        next(error);
+            { new: true }
+          );
+          const { password, ...rest } = updatedUser._doc;
+          res.status(200).json(rest);
+        } catch (error) {
+          next(error);
       }
     }
-  };
+  
