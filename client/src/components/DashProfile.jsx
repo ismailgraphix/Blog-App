a62ed49cbfd1f888858266c77aa5bd528acca270
ismailgraphix <ismailgrapix@gmail.com>
2024-02-23 13:14:39 +0100

    add create post api route

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -21,8 +21,10 @@ import {
   } from '../redux/user/userSlice';
   import { useDispatch } from 'react-redux';
   import { HiOutlineExclamationCircle } from 'react-icons/hi';
+  import { Link } from 'react-router-dom';
+
 export default function DashProfile() {
-  const { currentUser, error } = useSelector((state) => state.user);
+  const { currentUser, error, loading } = useSelector((state) => state.user);
     const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -231,9 +233,26 @@ export default function DashProfile() {
               onChange={handleChange}
           />
          
-          <Button type='submit' gradientDuoTone='purpleToBlue' outline>
+         <Button
+          type='submit'
+          gradientDuoTone='purpleToBlue'
+          outline
+          disabled={loading || imageFileUploading}
+        >
+          {loading ? 'Loading...' : 'Update'}
           Update
           </Button>
+          {currentUser.isAdmin && (
+          <Link to={'/create-post'}>
+            <Button
+              type='button'
+              gradientDuoTone='purpleToPink'
+              className='w-full'
+            >
+              Create a post
+            </Button>
+          </Link>
+        )}
         </form>
         <div className='text-red-500 flex justify-between mt-5'>
         <span onClick={() => setShowModal(true)} className='cursor-pointer'>
